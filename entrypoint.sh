7181e0a56c9129f6cb5d92f786237384098468b4
Steve Peak <steve@stevepeak.net>
2018-04-10 14:57:20 +0200

    change to file box

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,43 @@
 #!/bin/sh
 set -e
 
-echo $1 > $2
+if [ "$1" = "write" ]; then
+  echo $2 > $3
+
+  echo "{
+    what: filesize
+    path: $3
+    size: $(stat -c'%s' $3)
+    metric_type: guage
+  }" > /tmp/metrics/2.0
+
+else if [ "$1" = "read" ]; then
+  echo "{
+    what: filesize
+    path: $2
+    size: $(stat -c'%s' $2)
+    metric_type: guage
+  }" > /tmp/metrics/2.0
+
+  cat $2
+
+else if [ "$1" = "size" ]; then
+  stat -c'%s' $2
+
+else if [ "$1" = "exists" ]; then
+  if [ -f $2 ] || [ -d $2 ]; then
+     echo 'true'
+  else
+    echo 'false'
+  fi
+
+else if [ "$1" = "type" ]; then
+  if [ -f $2 ]; then
+    echo 'file'
+  else if [ -f $2 ]; then
+    echo 'folder'
+  else
+    exit 0
+  fi
+
+fi
