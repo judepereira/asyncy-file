e02c7b911cef07752f1258602eb21dae239c2d70
Jude Pereira <judebpereira@gmail.com>
2018-04-25 21:53:11 +0530

    Fixed branching; commented out the metrics

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,37 +4,37 @@ set -e
 if [ "$1" = "write" ]; then
   echo $2 > $3
 
-  echo "{
-    what: filesize
-    path: $3
-    size: $(stat -c'%s' $3)
-    metric_type: guage
-  }" > /tmp/metrics/2.0
+#  echo "{
+#    what: filesize
+#    path: $3
+#    size: $(stat -c'%s' $3)
+#    metric_type: guage
+#  }" > /tmp/metrics/2.0
 
-else if [ "$1" = "read" ]; then
-  echo "{
-    what: filesize
-    path: $2
-    size: $(stat -c'%s' $2)
-    metric_type: guage
-  }" > /tmp/metrics/2.0
+elif [ "$1" = "read" ]; then
+#  echo "{
+#    what: filesize
+#    path: $2
+#    size: $(stat -c'%s' $2)
+#    metric_type: guage
+#  }" > /tmp/metrics/2.0
 
   cat $2
 
-else if [ "$1" = "size" ]; then
+elif [ "$1" = "size" ]; then
   stat -c'%s' $2
 
-else if [ "$1" = "exists" ]; then
+elif [ "$1" = "exists" ]; then
   if [ -f $2 ] || [ -d $2 ]; then
      echo 'true'
   else
     echo 'false'
   fi
 
-else if [ "$1" = "type" ]; then
+elif [ "$1" = "type" ]; then
   if [ -f $2 ]; then
     echo 'file'
-  else if [ -f $2 ]; then
+  elif [ -f $2 ]; then
     echo 'folder'
   else
     exit 0
